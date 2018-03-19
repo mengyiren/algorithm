ff3883e885b8b98ac23f1dae59506e3e01033a97
mengyiren <mengyiren@renrenche.com>
2018-03-19 17:06:17 +0800

    反转三位数

--- java/src/main/java/com/lintcode/algorithm/ReversedNumber.java
+++ java/src/main/java/com/lintcode/algorithm/ReversedNumber.java
@@ -1,13 +1,22 @@
 package com.lintcode.algorithm;
 
+import org.junit.Test;
+
 /**
  * Created by IntelliJ IDEA.
  * User: mengyiren
  * Date: 2018/3/19
  */
 public class ReversedNumber {
-    int reverseInteger(int number){
-        
-        return 0;
+    int  reverseInteger(int number) {
+        int bai = number / 100;
+        int shi = number % 100 / 10;
+        int ge = number % 10;
+        return ge*100+shi*10+bai;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(reverseInteger(900));
     }
 }
