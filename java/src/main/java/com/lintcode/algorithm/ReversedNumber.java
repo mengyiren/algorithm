55cae2890dcf600679457afb6dda137424f3d42f
mengyiren <mengyiren@renrenche.com>
2018-03-19 11:11:46 +0800

    初始化

---
+++ java/src/main/java/com/lintcode/algorithm/ReversedNumber.java
@@ -0,0 +1,13 @@
+package com.lintcode.algorithm;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mengyiren
+ * Date: 2018/3/19
+ */
+public class ReversedNumber {
+    int reverseInteger(int number){
+        
+        return 0;
+    }
+}
