54316f634a0cb6569bbba0e2764b6fa2e3c3cc5c
mengyiren <mengyiren@renrenche.com>
2018-03-23 12:34:15 +0800

    二叉树中搜索区间

---
+++ java/src/main/java/com/lintcode/algorithm/SearchRange.java
@@ -0,0 +1,50 @@
+package com.lintcode.algorithm;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mengyiren
+ * Date: 2018/3/23
+ */
+public class SearchRange {
+    /**
+     * @param root: param root: The root of the binary search tree
+     * @param k1:   An integer
+     * @param k2:   An integer
+     * @return: return: Return all keys that k1<=key<=k2 in ascending order
+     */
+    public List<Integer> searchRange(TreeNode root, int k1, int k2) {
+        List<Integer> result = new LinkedList<>();
+        search(result,root,k1,k2);
+        return result;
+    }
+
+    private void search(List<Integer> result, TreeNode root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+        if (root.val > k1) {
+            search(result,root.left,k1,k2);
+        }
+        if (root.val >= k1 && root.val <= k2) {
+            result.add(root.val);
+        }
+        if (root.val < k2) {
+            search(result,root.right,k1,k2);
+        }
+    }
+}
+
+class TreeNode {
+    public int val;
+    public TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
