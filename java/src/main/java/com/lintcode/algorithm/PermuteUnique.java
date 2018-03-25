38e5795048ef9640c969377fe063e92932d98612
mengyiren <329274258@qq.com>
2018-03-25 23:45:09 +0800

    带重复元素的排列

---
+++ java/src/main/java/com/lintcode/algorithm/PermuteUnique.java
@@ -0,0 +1,46 @@
+package com.lintcode.algorithm;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PermuteUnique {
+    /*
+     * @param :  A list of integers
+     * @return: A list of unique permutations
+     */
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (nums == null) {
+            return null;
+        }
+        if (nums.length == 0) {
+            result.add(new LinkedList<>());
+            return result;
+        }
+        List<Integer> newList = new LinkedList<>();
+        newList.add(nums[0]);
+        result.add(newList);
+        int sizeOne, sizeTwo;
+        for (int i = 1; i < nums.length; i++) {
+            sizeOne = result.size();
+            for (int j = 0; j < sizeOne; j++) {
+                sizeTwo = result.get(0).size();
+                int count = 0;
+                for (int k = 0; k <= sizeTwo; k++) {
+                    newList = new LinkedList<>(result.get(0));
+                    newList.add(k, nums[i]);
+                    result.add(newList);
+                }
+                result.remove(0);
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(permuteUnique(new int[]{1, 2, 2}));
+    }
+}
