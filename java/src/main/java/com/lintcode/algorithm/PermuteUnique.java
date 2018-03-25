d562c1a3ab64326c5f03168223cd997b160635f5
mengyiren <329374258@qq.com>
2018-03-26 01:53:05 +0800

    带重复元素的排列

--- java/src/main/java/com/lintcode/algorithm/PermuteUnique.java
+++ java/src/main/java/com/lintcode/algorithm/PermuteUnique.java
@@ -2,9 +2,15 @@ package com.lintcode.algorithm;
 
 import org.junit.Test;
 
+import java.security.interfaces.RSAKey;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * 从第一个数开始，分别与它后面的不重复的数字交换
+ * 使用编程语言来说就是交换i与j的时候，要求[i,j)中间没有与第j个数字相同的数字
+ */
 public class PermuteUnique {
     /*
      * @param :  A list of integers
@@ -20,20 +26,31 @@ public class PermuteUnique {
             return result;
         }
         List<Integer> newList = new LinkedList<>();
-        newList.add(nums[0]);
+        for (int num : nums) {
+            newList.add(num);
+        }
         result.add(newList);
-        int sizeOne, sizeTwo;
-        for (int i = 1; i < nums.length; i++) {
-            sizeOne = result.size();
-            for (int j = 0; j < sizeOne; j++) {
-                sizeTwo = result.get(0).size();
-                int count = 0;
-                for (int k = 0; k <= sizeTwo; k++) {
-                    newList = new LinkedList<>(result.get(0));
-                    newList.add(k, nums[i]);
-                    result.add(newList);
+        int count = 0, sum = result.size();
+        while (count < sum) {
+            boolean canExchange = true;
+            newList = result.get(count);
+            for (int i = count++; i < newList.size() - 1; i++) {
+                for (int j = i + 1; j < newList.size(); j++) {
+                    for (int k = i; k < j; k++) {
+                        if (Objects.equals(newList.get(k), newList.get(j))) {
+                            canExchange = false;
+                            break;
+                        }
+                    }
+                    if (canExchange) {
+                        List<Integer> list = new LinkedList<>();
+                        list.addAll(newList);
+                        list.set(i, newList.get(j));
+                        list.set(j, newList.get(i));
+                        result.add(list);
+                        sum = result.size();
+                    }
                 }
-                result.remove(0);
             }
         }
         return result;
@@ -41,6 +58,6 @@ public class PermuteUnique {
 
     @Test
     public void test() {
-        System.out.println(permuteUnique(new int[]{1, 2, 2}));
+        System.out.println(permuteUnique(new int[]{1, 1, 2}));
     }
 }
