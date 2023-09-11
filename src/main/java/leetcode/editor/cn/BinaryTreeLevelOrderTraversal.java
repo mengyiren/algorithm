9aab4d0a099b250304616cacaf61b0fe94fe829c
mengyiren <329274258@qq.com>
2023-09-11 18:19:54 +0800

    二叉树的层序遍历

--- src/main/java/leetcode/editor/cn/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/leetcode/editor/cn/BinaryTreeLevelOrderTraversal.java
@@ -35,8 +35,8 @@
 
 package leetcode.editor.cn;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -70,19 +70,19 @@ public class BinaryTreeLevelOrderTraversal {
     class Solution {
         public List<List<Integer>> levelOrder(TreeNode root) {
             List<List<Integer>> res = new ArrayList<>();
-            if (root == null) {
-                return res;
+
+            Queue<TreeNode> queue = new ArrayDeque<>();
+            if (root != null) {
+                queue.add(root);
             }
-            Queue<TreeNode> queue = new LinkedList<>();
-            queue.offer(root);
             while (!queue.isEmpty()) {
+                int n = queue.size();
                 List<Integer> level = new ArrayList<>();
-                int size = queue.size();
-                for (int i = 0; i < size; i++) {
+                for (int i = 0; i < n; i++) {
                     TreeNode node = queue.poll();
                     level.add(node.val);
                     if (node.left != null) {
-                        queue.offer(node.left);
+                        queue.add(node.left);
                     }
                     if (node.right != null) {
                         queue.add(node.right);
