dc8297b621c82cffec8b99610830a4842abed887
mengyiren <329274258@qq.com>
2023-09-13 16:20:46 +0800

    岛屿数量

---
+++ src/main/java/leetcode/editor/cn/NumberOfIslands.java
@@ -0,0 +1,92 @@
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。 
+//
+// 此外，你可以假设该网格的四条边均被水包围。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 300 
+// grid[i][j] 的值为 '0' 或 '1' 
+// 
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集 数组 矩阵 👍 2293 👎 0
+
+
+package leetcode.editor.cn;
+
+public class NumberOfIslands {
+    public static void main(String[] args) {
+        Solution solution = new NumberOfIslands().new Solution();
+        solution.numIslands(new char[][]{{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}});
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numIslands(char[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+            int row = grid.length;
+            int col = grid[0].length;
+            int ans = 0;
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (grid[i][j] == '1') {
+                        ans++;
+                        dfs(grid, i, j);
+                    }
+                }
+            }
+            return ans;
+        }
+
+        private void dfs(char[][] grid, int row, int col) {
+            if (!inArea(grid, row, col) || grid[row][col] == '0') {
+                return;
+            }
+            grid[row][col] = '0';
+            dfs(grid, row + 1, col);
+            dfs(grid, row - 1, col);
+            dfs(grid, row, col + 1);
+            dfs(grid, row, col - 1);
+        }
+
+        private boolean inArea(char[][] grid, int row, int col) {
+            return row >= 0 && row < grid.length && col >= 0 && col < grid[0].length;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
