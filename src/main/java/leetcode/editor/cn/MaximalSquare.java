12e04f53d4e49cd5524d56530c1f4bcb0b7b0828
mengyiren <329274258@qq.com>
2023-09-14 15:46:45 +0800

    最大正方形

--- src/main/java/leetcode/editor/cn/MaximalSquare.java
+++ src/main/java/leetcode/editor/cn/MaximalSquare.java
@@ -52,25 +52,26 @@ public class MaximalSquare {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maximalSquare(char[][] matrix) {
-            int m = matrix.length;
-            int n = matrix[0].length;
-            int max = 0;
-            int[][] dp = new int[m][n];
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (matrix[i][j] == '0') {
-                        dp[i][j] = 0;
-                    } else {
+            int maxSide = 0;
+            if (matrix == null || matrix.length == 0) {
+                return maxSide;
+            }
+            int rows = matrix.length;
+            int cols = matrix[0].length;
+            int[][] dp = new int[rows][cols];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    if (matrix[i][j] == '1') {
                         if (i == 0 || j == 0) {
                             dp[i][j] = 1;
                         } else {
-                            dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
+                            dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
                         }
+                        maxSide = Math.max(maxSide, dp[i][j]);
                     }
-                    max = Math.max(dp[i][j], max);
                 }
             }
-            return max * max;
+            return maxSide * maxSide;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
